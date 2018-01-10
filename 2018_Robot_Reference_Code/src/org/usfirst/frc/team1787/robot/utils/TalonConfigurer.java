08c7b2ff55291d710659739431ec89e306dfcad7
Simon Wieder <Simon Wieder@Simons-Mac-Running-Windows>
2018-01-09 23:18:54 -0500

    Added documentation outlining new talon config options!

--- 2018_Robot_Reference_Code/src/org/usfirst/frc/team1787/robot/utils/TalonConfigurer.java
+++ 2018_Robot_Reference_Code/src/org/usfirst/frc/team1787/robot/utils/TalonConfigurer.java
@@ -4,34 +4,59 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class TalonConfigurer {
+	
+	public static final int DEFAULT_TALON_FUNCTION_TIMEOUT_MS = 10;
   
   /**
    * Configs the given talon as follows
-   * 1) changes control mode to Voltage control, 
-   * which allows you to specify the voltage that the talon should apply to the motor.
-   * 
-   * 2) enables break mode
-   * 
-   * 3) sets the maximum voltage ramp rate
-   * 
-   * 4) sets a current limit
-   * @param talon
+   * 1) Sets inverted to false
+   * 2) Sets brake mode to enabled
+   * 3) Sets current limit to default
+   * 4) Sets voltage ramp rate minimum to default
+   * 5) Enables voltage compensation mode, with a max voltage of 12 volts
+   * @param talon the talon to configure.
    */
   public static void configTalon(WPI_TalonSRX talon) {
+    // can be set to true if positive values make the motor turn backwards
+    talon.setInverted(false);
+    
+    // determines what the motor does (either brake or coast) when an output of 0 is sent to the motor controller
     talon.setNeutralMode(NeutralMode.Brake);
     
-    // setting a ramp rate of 0 indicates that no max ramp rate will be enforced.
-    //talon.setVoltageRampRate(0);
+    // The above 2 config settings are the only config settings that need to be set on a "follower" talon.
+    // all other config settings will be inherited from the master talon.
     
-    //talon.setCurrentLimit(0);
-    //talon.EnableCurrentLimit(false);
+    // Current Limiting
+    /* There are 3 different parameters that are important regarding current limiting.
+     * 1) peakCurrentLimit: this is the max amount of current (in amps) that the motor controller will give to the motor.
+     * 2) peakCurrentDuration: this is the max amount of time (in ms) that the peakCurrentLimit will be applied to the motor. 
+     * 3) continuousCurrentLimit: this is the "fallback" current that will be applied to a motor 
+     * that has been drawing the peakCurrentLimit for the peakCurrentDuration. 
+     * Note: If the peakCurrentLimit is set to 0, then the continuousCurrentLimit will be enforced at all times. */
+    talon.configPeakCurrentLimit(0, DEFAULT_TALON_FUNCTION_TIMEOUT_MS);
+    talon.configPeakCurrentDuration(0, DEFAULT_TALON_FUNCTION_TIMEOUT_MS);
+    talon.configContinuousCurrentLimit(0, DEFAULT_TALON_FUNCTION_TIMEOUT_MS);
+    talon.enableCurrentLimit(false);
     
-    //talon.changeControlMode(WPI_TalonSRX.TalonControlMode.Voltage);
-    // Note: was gonna try Voltage control mode, as it appeared it would be more consistent, 
-    // but the wpilib PIDController
-    // requires PercentVbus, which is the default control mode of the talon.
-    // It takes in a number between -1 and 1 and sets the voltage to be that percentage.
-    //talon.changeControlMode(WPI_TalonSRX.TalonControlMode.PercentVbus);
+    // Voltage Ramp Rate
+    /* This sets constraints on the rate at which the voltage applied can change. This is to help prevent
+     * instantaneous changes in voltage that may harm the motor. 
+     * parameter: (minimumSecondsFromNeutralToFull, timeoutForChangingThisSettingInMs) 
+     * timeoutForChanging this setting = the max time allowed for the talon to change this setting. 
+     * Note: set the minSecondsFromNeutralToFull to 0 to enforce no ramp rate. */
+    talon.configOpenloopRamp(0, DEFAULT_TALON_FUNCTION_TIMEOUT_MS);
+    
+    // Voltage Compensation
+    /* helps maintain consistency despite changing battery voltage. */
+    talon.configNominalOutputForward(0, DEFAULT_TALON_FUNCTION_TIMEOUT_MS);
+    talon.configNominalOutputReverse(0, DEFAULT_TALON_FUNCTION_TIMEOUT_MS);
+    talon.configPeakOutputForward(1, DEFAULT_TALON_FUNCTION_TIMEOUT_MS);
+    talon.configPeakOutputReverse(-1, DEFAULT_TALON_FUNCTION_TIMEOUT_MS);
+    
+    // Sets max voltage for voltage compensation mode.
+    talon.configVoltageCompSaturation(12, DEFAULT_TALON_FUNCTION_TIMEOUT_MS);
+    talon.enableVoltageCompensation(true);
+     
     talon.set(0);
   }
   
@@ -39,7 +64,7 @@ public class TalonConfigurer {
   /* talon is constructed with default update rate of 10ms
    * if a different update rate is desired, it can be set as a 2nd 
    * parameter to the constructor. The given # will be the update rate in ms.
-   * note that increasing this rate will increase bandwith. leaving it at the deafault is probably best
-   * for now until we have time to research / test the consequences of chaning it.
+   * note that increasing this rate will increase bandwidth. leaving it at the default is probably best
+   * for now until we have time to research / test the consequences of changing it.
    */
 }
